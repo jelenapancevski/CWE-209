259f6bf38e9a1ddcbc869c27e450bfd79dac3716
Jelena Pancevski <pj180123d@student.etf.bg.ac.rs>
2023-12-25 14:00:11 +0100

    Fix the visuals of promotion pls

--- cwe209/src/main/java/com/rbs/cwe209/controller/PromotionsController.java
+++ cwe209/src/main/java/com/rbs/cwe209/controller/PromotionsController.java
@@ -1,10 +1,12 @@
 package com.rbs.cwe209.controller;
 
+import com.rbs.cwe209.model.Product;
 import com.rbs.cwe209.model.Promotion;
 import com.rbs.cwe209.repository.PromotionRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -22,5 +24,15 @@ public class PromotionsController {
         List<Promotion> promotions = promotionRepository.getAll();
         model.addAttribute("promotions",promotions);
         return "promotions";
+    }
+    @GetMapping("/promotion/{id}")
+    public String getPromotion(Model model,@PathVariable Long id) {
+
+        Promotion promotion = promotionRepository.getPromotion(id);
+        if(promotion==null){
+            model.addAttribute("errorMessage","No product with id = "+id);
+        }
+        model.addAttribute("promotion",promotion);
+        return "promotion";
     }
 }
--- cwe209/src/main/java/com/rbs/cwe209/repository/PromotionRepository.java
+++ cwe209/src/main/java/com/rbs/cwe209/repository/PromotionRepository.java
@@ -36,4 +36,18 @@ public class PromotionRepository {
         String image = rs.getString(4);
         return new Promotion(id, name,description,image);
     }
+    public Promotion getPromotion(Long id) {
+        String sqlQuery = "SELECT * FROM promotions WHERE id="+id;
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet rs = statement.executeQuery(sqlQuery)) {
+            rs.next();
+            return (createPromotion(rs));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+        return null;
+    }
 }
